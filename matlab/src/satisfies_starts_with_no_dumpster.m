fa7a7442e0f55ac53908ac52a59c7c712a60488b
Trever Hallock <this.address.does.not.exist@gmail.com>
2014-10-09 19:20:33 -0600

    I forgot what I did.

---
+++ matlab/src/satisfies_starts_with_no_dumpster.m
@@ -0,0 +1,12 @@
+% This function makes sure that the first operation that a truck performs has no dumpster.
+% This is because we assume that each truck starts out with no dumpster.
+% The set of first possible operations is: {'U',  'P' }
+
+
+% author: Tamlyn
+
+function [is_valid] = satisfies_starts_with_no_dumpster(c, sol)
+
+is_valid = true;
+
+end
--- matlab/src/satisfies_time_windows.m
+++ matlab/src/satisfies_time_windows.m
@@ -4,7 +4,12 @@
 
 % author     <not assigned>
 
-function [is_valid] = satisfies_time_windows(c, sol)
+
+% c is the city
+% sol is the solution
+% all_times is the time that each action represented by sol is performed (taken from get_all_times.m)
+
+function [is_valid] = satisfies_time_windows(c, sol, all_times)
 
 is_valid = true;
 
