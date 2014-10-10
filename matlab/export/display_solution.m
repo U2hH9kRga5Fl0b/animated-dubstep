471cc662caea6f4a71b197da0df7b09a2aa94b4f
hallock <me@example.com>
2014-10-09 23:22:52 -0600

    Tried to make a display_solution function. but failed :(

--- matlab/export/display_solution.m
+++ matlab/export/display_solution.m
@@ -1,88 +1,29 @@
 
-% Now, to display the city in a visualization overview
+% Now, to display the solution in a visualization overview
 
-% This function plots a city.
+% This function plots a solution over a city.
+
+% This looks like garbage :(
 
 function display_solution(c, sol)
-% TODO: this function shouldn't depend on the order the city was made!!!
 
-sizes = 3 * ones(c.number_of_locations, 1);
-colors = ones(c.number_of_locations, 1);
+display_city(c);
 
-for i=1:c.number_of_staging_areas
-    colors(i, 1) = 4;
-end
-for i=(1+c.number_of_staging_areas):(c.number_of_drivers + c.number_of_staging_areas)
-    colors(i, 1) = 7;
-end
+colors = rand(c.number_of_drivers, 3);
 
-%need starts, roads, wait times, sizes, number of trucks
-scatter(c.locs(:, 1), c.locs(:, 2), sizes, colors);
-
-for i = 1:c.number_of_staging_areas
-    %List the yards
-    loc = c.locs(c.yards(i).location, :);
-    if (i > 1)
-        text(loc(1, 1), loc(1, 2), ...
-            0, strcat(' Yard #', int2str(i)),...
-            'VerticalAlignment', 'bottom', 'Color', 'green');
+for d=1:c.number_of_drivers
+    prev = c.locs(c.start_location, :);
+    for s=1:c.number_of_actions
+        if sol(d,s) < 0
+            break;
+        end
+        
+        next = c.locs(c.actions(sol(d,s)).location, :);
+        
+        line(prev, next, 'Color', colors(d, :));
+        prev = next;
     end
-    text(loc(1, 1), loc(1, 2), 0, 'YARD', ...
-        'HorizontalAlignment', 'right', ...
-        'VerticalAlignment', 'bottom', 'Color', 'green');
-    text(loc(1, 1), loc(1, 2), 0, ...
-        strcat('INV[', ... 
-        int2str(c.yards(i).inventory(1)), ', ', ...
-        int2str(c.yards(i).inventory(2)), ', ', ...
-        int2str(c.yards(i).inventory(3)), ', ', ...
-        int2str(c.yards(i).inventory(4)), '] '), ...
-        'HorizontalAlignment', 'right', 'VerticalAlignment', 'top', ...
-        'Color', 'green');
-    text(loc(1, 1), loc(1, 2), 0, ...
-        strcat(' CAP: ', int2str(c.yards(i).capacity)), ...
-        'HorizontalAlignment', 'left', 'VerticalAlignment', 'top',...
-        'Color', 'green');
-end
-for i = 1:c.number_of_landfills
-    %List the dumps
-%   If we wanted to print the loction it would be:
-%    l = c.landfills(i).location;
-    loc = c.locs(c.landfills(i).location, :);
-    text(loc(1, 1), loc(1, 2), 0, ' DUMP', ...
-        'VerticalAlignment', 'top', 'Color', 'red');
-    text(loc(1, 1), loc(1, 2), 0, strcat(' (Dump #', ...
-        int2str(i), ') '), 'VerticalAlignment', 'bottom', ...
-        'Color', 'red');
 end
 
 
-
-
-for i = (c.number_of_staging_areas + c.number_of_landfills + 1):c.number_of_locations
-    %List all of the customer requests
-    loc = c.locs(i, :);
-%    text(loc(1,1), loc(1,2), strcat(' ', c.actions(i).operation));
-    text(loc(1, 1), loc(1, 2), ...
-        strcat(' (Stop #', int2str(i - (c.number_of_staging_areas + c.number_of_landfills)), ') '), ...
-        'HorizontalAlignment', 'right');
-end
-
-%List the first yard
-loc = c.locs(c.yards(c.start_location).location, :);
-text(loc(1, 1), loc(1, 2), 0,...
-    '(Start) #1', 'VerticalAlignment', 'bottom', 'Color', 'green');
-
-end
-
-% TODO:
-
-
-%Create the roads
-% NOTE: I've commented this out, because it clutters up the graph;
-% however, the logic works
-%for i = 1:(Y + D + n)
-%    for j = 1:(Y + D + n)
-%        line( [ locs(i, 1), locs(j, 1) ], [ locs(i, 2), locs(j, 2) ] );
-%    end
-%end
 end
