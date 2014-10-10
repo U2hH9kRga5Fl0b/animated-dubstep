d0ad3e63b6a5e96096ce794cd8f642bd86295d5e
Trever Hallock <this.address.does.not.exist@gmail.com>
2014-10-09 18:11:35 -0600

    updated example code

--- matlab/example/example.m
+++ matlab/example/example.m
@@ -1,90 +1,58 @@
 
-
-
-
-
-
-
-
-
+% Make a city
 c = generate_city(10,2,2,2)
 
 
 
 
+% Make a solution, although it probably isn't valid.
+sol = generate_rand_solution(c)
 
-sol = cast(1 + (c.n-1) * rand(c.D, c.n), 'int32')
 
 
 
+% This is the number of actions
+c.n
 
-first_operation
 
 
 
+% This is the number of drivers
+c.D
 
 
 
 
+% These are the actions
+c.actions
 
 
 
 
+% Look at the 5-th action
+c.actions(5)
 
 
 
 
+% What is the 5-th action's operation
+c.actions(5).operation
 
 
 
 
+% What is the third action performed by the second driver
+c.actions(sol(2,3));
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% Now c has 50 actions
 c = generate(50,4,3, 3);
-c.actions(1)
-c.action(50)
-sol = cast( max(1,c.n * rand(c.D, c.n)), 'uint32');
-get_all_times(c, sol);
+
+
+
+
+% This is not working yet, but what happens when we try to simulate a solution?
+simulate(c, generate_rand_solution(c));
+
