8635f2691874f02aae3fa12dd8e3eb458ff8e91c
Trever Hallock <this.address.does.not.exist@gmail.com>
2014-10-14 09:40:31 -0600

    Stuff for Tamlyn. I changed a couple things: fprintf --> warning and to only assign is_valid to false (and not back to true again).

--- matlab/src/satisfies_starts_with_no_dumpster.m
+++ matlab/src/satisfies_starts_with_no_dumpster.m
@@ -9,4 +9,23 @@ function [is_valid] = satisfies_starts_with_no_dumpster(c, sol)
 
 is_valid = true;
 
+for d = 1:c.number_of_drivers
+    if sol(d,1) < 0
+        continue;
+    end
+    % First operation for each driver
+    s = c.actions(sol(d,1)).operation;
+    % 'U' and 'P' only possible first operations
+    if s ~= 'U' && s ~= 'P'
+        is_valid = false;
+        warning(['Driver %d''s first operation is %s.\n'...
+            '   The first operation cannot have a dumpster, so it can only be a P or U.\n\n'], d, s); 
+    end
+%    if c.actions(sol(d,1)).insize ~= 0
+%        is_valid = false;
+%        fprintf(['Driver %d''s first operation is %s.\n'...
+%            '   The first operation cannot have a dumpster, so it can only be a P or U.\n\n'], d, s); 
+%    end
+end
+
 end
