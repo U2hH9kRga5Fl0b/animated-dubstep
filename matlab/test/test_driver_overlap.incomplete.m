de73ce6e3523a88c11fa3466a6be1226f6c8a1bf
hallock <me@example.com>
2014-10-22 19:20:15 -0600

    Adding matt's tests

---
+++ matlab/test/test_driver_overlap.incomplete.m
@@ -0,0 +1,18 @@
+
+
+c=generate_city(50, 2, 2, 2);
+sol=generate_rand_solution(c); % Create a random solution
+%Make sure that one of the entires overlaps
+a = 0;
+for i = 1:c.number_of_actions
+    if c.actions(i).operation == 'P' || c.actions(i).operation == 'D' || c.actions(i).operation == 'R'
+        a = i;
+        break;
+    end
+end
+sol(1,1) = a;
+sol(1,2) = a;
+[feasible, times, distances, num_serviced, fees] = simulate(c, sol);
+
+% Verify some of the obvious facts about the empty solution...
+assert(feasible, 'The driver visited a stop more than once!');
---
+++ matlab/test/test_time_windows.incomplete.m
@@ -0,0 +1,13 @@
+
+
+c=generate_city(50, 2, 2, 2);
+sol=generate_rand_solution(c); % Create a random solution
+%Make sure that one time window cannot be met
+
+c.actions(1).start_time = 0;
+c.actions(1).stop_time = 0;
+
+[feasible, times, distances, num_serviced, fees] = simulate(c, sol);
+
+% Verify some of the obvious facts about the empty solution...
+assert(feasible, 'The time window on stop 1 was not met.');
